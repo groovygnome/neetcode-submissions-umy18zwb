962060671253980c9181073847148e50864fac51
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-04-02 08:48:26 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let answers = new Map();
+        for(let i = 0; i < strs.length; i++){
+            let charCount = new Array(26).fill(0);
+            for(let j = 0; j < strs[i].length; j++){
+                charCount[strs[i].charCodeAt(j) % 26] += 1;
+            }
+            console.log(charCount);
+            let sortStr = charCount.join('#');
+            console.log(sortStr);
+            if(!answers.get(sortStr)) answers.set(sortStr, []);
+            answers.get(sortStr).push(strs[i]);
+        }
+        return answers.values().toArray();
+    }
+}

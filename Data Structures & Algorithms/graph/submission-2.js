1097d172f5231c397129cf9248cc8d514157f121
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-04-08 11:38:29 -0400

    Add: graph - submission-2

---
+++ Data Structures & Algorithms/graph/submission-2.js
@@ -0,0 +1,90 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.edges = new Map();
+    }
+
+    getEdges(){
+        return this.edges;
+    }
+
+    addEdge(node){
+        this.edges.set(node.val, node);
+    }
+
+    getEdge(val){
+        return this.edges.get(val);
+    }
+
+    removeEdge(val){
+        if(this.edges.get(val) === undefined){
+            return false;
+        } else{ 
+            this.edges.delete(val);
+            return true;
+        }
+    }
+}
+
+class Graph {
+    constructor() {
+        this.nodes = new Map();
+    }
+
+    /**
+     * @param {number} src
+     * @param {number} dst
+     * @return {void}
+     */
+    addEdge(src, dst) {
+        let source = this.nodes.get(src);
+        if(source === undefined){
+            this.nodes.set(src, new Node(src));
+            source = this.nodes.get(src);
+        }
+        let dest = this.nodes.get(dst);
+        if(dest === undefined){
+            this.nodes.set(dst, new Node(dst));
+            dest = this.nodes.get(dst);
+        }
+        if(source.getEdge(dst) === undefined) source.addEdge(dest);
+    }
+
+    /**
+     * @param {number} src
+     * @param {number} dst
+     * @return {boolean}
+     */
+    removeEdge(src, dst) {
+        let source = this.nodes.get(src);
+        let dest = this.nodes.get(dst);
+        if(source === undefined || dest === undefined) return false;
+        return source.removeEdge(dst);
+    }
+
+    /**
+     * @param {number} src
+     * @param {number} dst
+     * @return {boolean}
+     */
+    hasPath(src, dst, visited = new Set()) {
+        if(visited.has(src)) return false;
+        if(src === dst) return true;
+        let source = this.nodes.get(src);
+        let edges = source.getEdges();
+        if(source === undefined || edges.size === 0 || this.nodes.get(dst) === undefined) return false;
+
+        visited.add(src);
+
+        let hasPath = false;
+
+        let keys = edges.keys().toArray();
+
+        for(let i = 0; i < keys.length; i++){
+            if(hasPath) return hasPath;
+            hasPath = hasPath || this.hasPath(keys[i], dst, visited);
+        }
+
+        return hasPath;
+    }
+}

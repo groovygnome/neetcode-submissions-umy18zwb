d505e720b00db114d2d8ec4bfac88b48ac070744
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-04-11 11:09:19 -0400

    Add: graph - submission-8

---
+++ Data Structures & Algorithms/graph/submission-8.js
@@ -0,0 +1,68 @@
+class Graph {
+    constructor() {
+        this.nodes = new Map();
+    }
+
+    /**
+     * @param {number} src
+     * @param {number} dst
+     * @return {void}
+     */
+    addEdge(src, dst) {
+        if(!this.nodes.get(src)){
+            this.nodes.set(src, new Set());
+        }
+        if(!this.nodes.get(dst)){
+            this.nodes.set(dst, new Set());
+        }
+        this.nodes.get(src).add(dst);
+        console.log(this.nodes);
+    }
+
+    /**
+     * @param {number} src
+     * @param {number} dst
+     * @return {boolean}
+     */
+    removeEdge(src, dst) {
+        if(!this.nodes.get(src) || !this.nodes.get(dst)){
+            return false;
+        }
+        this.nodes.get(src).delete(dst);
+        return true;
+    }
+
+    /**
+     * @param {number} src
+     * @param {number} dst
+     * @return {boolean}
+     */
+    hasPath(src, dst) {
+
+        let visited = new Set();
+        let queue = [];
+        console.log(this.nodes.get(dst));
+        if(this.nodes.get(src) === undefined || this.nodes.size === 0 || this.nodes.get(dst) === undefined) return false;
+
+        queue.push(src);
+        visited.add(src);
+
+        while(queue.length > 0){
+            let queueLength = queue.length;
+            for(let i = 0; i < queueLength; i++){
+                let curr = queue.shift();
+                if(curr === dst) return true;
+                let currList = this.nodes.get(curr);
+                currList.forEach((node) =>{
+                    if(!visited.has(node)){
+                        queue.push(node);
+                        visited.add(curr);
+                    }
+                })
+
+            }
+        }
+
+        return false;
+    }
+}

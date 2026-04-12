ba8865e52393b3d51c0e6a680c62244bb540cd49
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-04-12 15:48:08 -0400

    Add: clone-graph - submission-4

---
+++ Data Structures & Algorithms/clone-graph/submission-4.js
@@ -0,0 +1,48 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        if(!node) return null;
+        let adjMap = new Map();
+
+        let nodeQueue = new Queue();
+        nodeQueue.enqueue(node);
+
+        while(nodeQueue.size() > 0){
+            let queueLength = nodeQueue.size();
+            for(let i = 0; i < queueLength; i++){
+                let curr = nodeQueue.dequeue();
+                if(!adjMap.get(curr.val)){
+                    adjMap.set(curr.val, new Node(curr.val))
+                }
+                let newNeighbors = [];
+                for(let j = 0; j < curr.neighbors.length; j++){
+                    if(!adjMap.get(curr.neighbors[j].val)){
+                        adjMap.set(curr.neighbors[j].val, new Node(curr.neighbors[j].val))
+                    }
+                    let newNeighbor = adjMap.get(curr.neighbors[j].val)
+                    newNeighbors.push(newNeighbor);
+                    if(newNeighbor.neighbors.length === 0){
+                        nodeQueue.enqueue(curr.neighbors[j]);
+                    }
+                }
+                adjMap.get(curr.val).neighbors = newNeighbors;
+
+            }
+        }
+
+        return adjMap.get(node.val);
+    }
+}

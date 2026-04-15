598930b8c9d1c49e399cc666b26384278381ec5f
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-04-15 13:54:22 -0400

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n, memo = new Map()) {
+        if(n < 0){
+            return 0;
+        }
+        if(n === 0){
+            return 1;
+        }
+        if(memo.get(n) != undefined){
+            return memo.get(n);
+        }
+        memo.set(n, (this.climbStairs(n-1, memo) + this.climbStairs(n-2, memo)))
+        return memo.get(n);
+    }
+}

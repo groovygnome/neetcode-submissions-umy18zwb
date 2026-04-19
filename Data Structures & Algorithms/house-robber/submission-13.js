9402e6bdae4a2a42a21a37c4b7af1a92aa986b7a
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-04-19 09:00:00 -0400

    Add: house-robber - submission-13

---
+++ Data Structures & Algorithms/house-robber/submission-13.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if(nums.length <= 2){
+            return Math.max(...nums);
+        }
+
+        let rob1 = nums[0];
+        let rob2;
+
+        if(nums[0] > nums[1]) rob2 = nums[0];
+        else rob2 = nums[1];
+
+        for(let i = 2; i < nums.length; i++){
+            let temp = Math.max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+
+        return rob2;
+        
+    }
+}

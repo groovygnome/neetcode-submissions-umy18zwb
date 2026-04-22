c87de90c8c39809fd6abd3b63d42b2b5d154107d
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-04-22 12:38:20 -0400

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+        let answerArray = [];
+        for(let i = 0; i < n; i++){
+            answerArray.push(1);
+        }
+        
+        for(let r = m-1; r > 0; r--){
+            console.log(answerArray);
+            for (let c = 0; c < n; c++){
+                answerArray[c] = answerArray.slice(c).reduce((total, current) => total + current, 0);
+            }
+        }
+        return answerArray[0];
+    }
+}

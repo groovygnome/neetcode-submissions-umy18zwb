d72c974edc88570d70875cfc153f4d62a8134ecb
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-05-01 17:30:16 -0400

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head, visited = new Set()) {
+        console.log(head);
+        if(head){
+            if(visited.has(head)) return true;
+            else {
+                visited.add(head);
+                return this.hasCycle(head.next, visited);
+            }
+        } else{
+            return false;
+        }
+    }
+}

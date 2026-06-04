29378a758f5795bd0f4ae79371af0dece520fc22
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-06-04 14:13:43 -0400

    Add: redundant-connection - submission-6

---
+++ Data Structures & Algorithms/redundant-connection/submission-6.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[][]} edges
+     * @return {number[]}
+     */
+    findRedundantConnection(edges) {
+        let parents = new Map();
+        let rank = new Map();
+
+        for (let i = 1; i < edges.length + 1; i++) {
+            parents.set(i, i);
+        }
+
+        let ans = [0, 0];
+
+        for (let i = 0; i < edges.length; i++) {
+            let p = edges[i][0];
+            let c = edges[i][1];
+
+            let pRoot = this.findRoot(p, parents);
+            let cRoot = this.findRoot(c, parents);
+
+            if (pRoot != cRoot) {
+                parents.set(pRoot, cRoot);
+            } else {
+                ans = edges[i];
+            }
+            console.log(parents);
+        }
+
+        return ans;
+    }
+
+    findRoot(curr, parents) {
+        while (curr != parents.get(curr)) {
+            curr = parents.get(curr);
+        }
+
+        return curr;
+    }
+}

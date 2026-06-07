3bd6b5ac29ac48c17196c38efec90383baf624c4
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-06-07 16:38:50 +0000

    Add: count-connected-components - submission-15

---
+++ Data Structures & Algorithms/count-connected-components/submission-15.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        let parents = new Map();
+        let ranks = new Map();
+
+        for(let i = 0; i < n; i++){
+            parents.set(i,i);
+            ranks.set(i, 1);
+        }
+
+        for(let i = 0; i < edges.length; i++){
+            let a = edges[i][0];
+            let b = edges[i][1];
+
+            let aRoot = this.findRoot(a, parents);
+            let bRoot = this.findRoot(b, parents);
+
+            if(aRoot != bRoot) n--;
+
+            if(ranks.get(aRoot) < ranks.get(bRoot)){
+                parents.set(aRoot, bRoot);
+            } else if(ranks.get(aRoot) > ranks.get(bRoot)){
+                parents.set(bRoot,aRoot);
+            } else if(ranks.get(aRoot) === ranks.get(bRoot)){
+                parents.set(aRoot,bRoot);
+                ranks.set(bRoot, ranks.get(bRoot)+1);
+            }
+            
+        }
+
+        return n;
+
+    }
+
+    findRoot(curr, parents){
+        while(curr != parents.get(curr)){
+            parents.set(curr, parents.get(parents.get(curr)));
+            curr = parents.get(curr);
+        }
+
+        return curr;
+    }
+}

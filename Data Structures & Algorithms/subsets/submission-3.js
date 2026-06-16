501d50cc56d69ff564ef6bdccdf53c063fc3fed3
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-06-16 17:14:07 +0000

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        let answer = [];
+        let curr = [];
+        this.helper(0, nums, curr, answer);
+        return answer;
+    }
+
+    helper(i, nums, curr, answer){
+        if(i >= nums.length){
+            answer.push(curr);
+            return;
+        }
+
+        curr.push(nums[i]);
+        this.helper(i+1, nums, [...curr], answer);
+        curr.pop();
+        this.helper(i+1, nums, [...curr], answer);
+    }
+}

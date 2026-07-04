604f547a4ee9d8d9cf2793c85a753a306882dd02
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-07-04 09:57:34 -0400

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        let prereqMap = new Map();
+        for(let i = 0; i < numCourses+1; i++){
+            prereqMap.set(i, []);
+        }
+        for(let i = 0; i < prerequisites.length; i++){
+            let array = prereqMap.get(prerequisites[i][0]);
+            array.push(prerequisites[i][1]);
+            prereqMap.set(prerequisites[i][0], array);
+        }
+        console.log(prereqMap);
+
+        let visited = new Set();
+        for(let i = 0; i < numCourses+1; i++){
+            if(!this.dfs(i, prereqMap, visited)) return false;
+        }
+
+        return true;
+    }
+
+    dfs(curr, prereqMap, visited, path = new Set()){
+        if(path.has(curr)) return false;
+        if(visited.has(curr)) return true;
+        visited.add(curr);
+        path.add(curr);
+
+        let neighbors = prereqMap.get(curr);
+        for(let neighbor of neighbors){
+            if(!this.dfs(neighbor, prereqMap, visited, path)) return false;
+        }
+
+        path.delete(curr);
+
+        return true;
+    }
+}

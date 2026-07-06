06bb6e0fa1dd08f7ff68ade8afb9358824e2a7b3
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-07-06 15:58:39 +0000

    Add: course-schedule-iv - submission-1

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-1.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @param {number[][]} queries
+     * @return {boolean[]}
+     */
+    checkIfPrerequisite(numCourses, prerequisites, queries) {
+        let prereqMap = new Map();
+        for(let i = 0; i < numCourses; i++){
+            prereqMap.set(i, []);
+        }
+        for(let i = 0; i < prerequisites.length; i++){
+            let array = prereqMap.get(prerequisites[i][1]);
+            array.push(prerequisites[i][0]);
+            prereqMap.set(prerequisites[i][1], array);
+        }
+
+        let answer = [];
+        for(let i = 0; i < queries.length; i++){
+            answer.push(this.dfs(queries[i][1], queries[i][0], prereqMap));
+        }
+
+        return answer;
+
+    }
+
+    dfs(curr, dest, prereqMap, path = new Set(), visited = new Set(), answer = false){
+        if(path.has(curr)) return false;
+        if(curr === dest) return true;
+        if(visited.has(curr)) return answer;
+
+        visited.add(curr);
+        path.add(curr);
+
+        let neighbors = prereqMap.get(curr);
+        for(let neighbor of neighbors){
+            answer = answer || this.dfs(neighbor, dest, prereqMap, path, visited, answer);
+        }
+        path.delete(curr);
+
+        return answer;
+    }
+}

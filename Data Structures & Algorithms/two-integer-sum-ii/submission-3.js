6baae6684d28c582d524032a26572bf628ab07c3
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-09-07 13:47:07 +0000

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let i = 0;
+        let j = nums.length-1;
+        while(i < j){
+            if(nums[i] + nums[j] < target){
+                i++;
+            } else if(nums[i] + nums[j] > target){
+                j--;
+            } else{
+                return [i+1, j+1];
+            }
+        }
+    }
+}

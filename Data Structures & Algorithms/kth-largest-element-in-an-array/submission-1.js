6a6399a54b38fb35d3fdc60c97b2f0f3059316f6
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-09-10 15:35:00 +0000

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.js
@@ -0,0 +1,81 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+        let maxHeap = new Heap(true);
+        for(let num of nums){
+            maxHeap.push(num);
+        }
+
+        console.log(maxHeap);
+        for(let i = 1; i < k; i++){
+            maxHeap.pop();
+        }
+
+        
+
+        return maxHeap.pop();
+    }
+}
+
+class Heap {
+    constructor(max) {
+        this.max = max;
+        this.heap = [-1];
+    }
+
+    push(val) {
+        this.heap.push(val);
+        let curr = this.heap.length - 1;
+        let p = Math.floor(curr / 2);
+        while (p > 0) {
+            if (this.max) {
+                if (this.heap[p] < this.heap[curr]) {
+                    [this.heap[p], this.heap[curr]] = [this.heap[curr], this.heap[p]];
+                    curr = p;
+                    p = Math.floor(curr / 2);
+                } else break;
+            } else {
+                if (this.heap[p] > this.heap[curr]) {
+                    [this.heap[p], this.heap[curr]] = [this.heap[curr], this.heap[p]];
+                    curr = p;
+                    p = Math.floor(curr / 2);
+                } else break;
+            }
+        }
+    }
+
+    pop() {
+        let ans = this.heap[1];
+        this.heap[1] = this.heap.pop();
+        let curr = 1;
+        let l = curr*2;
+        let r = l+1;
+        while (l < this.heap.length) {
+            if (this.max) {
+                let swap = l;
+                if(r < this.heap.length && this.heap[r] > this.heap[l]) swap = r;
+                if (this.heap[swap] > this.heap[curr]) {
+                    [this.heap[swap], this.heap[curr]] = [this.heap[curr], this.heap[swap]];
+                    curr = swap;
+                    l = curr*2;
+                    r = l+1;
+                } else break;
+            } else {
+                let swap = l;
+                if(r < this.heap.length && this.heap[r] < this.heap[l]) swap = r;
+                if (this.heap[swap] < this.heap[curr]) {
+                    [this.heap[swap], this.heap[curr]] = [this.heap[curr], this.heap[swap]];
+                    curr = swap;
+                    l = curr*2;
+                    r = l+1;
+                } else break;
+            }
+        }
+
+        return ans;
+    }
+}

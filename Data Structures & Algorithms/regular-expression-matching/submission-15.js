29fab21941372913c53d2f6e039a5d52d4e28011
John Perez <65876084+groovygnome@users.noreply.github.com>
2026-09-19 15:56:39 +0000

    Add: regular-expression-matching - submission-15

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-15.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} p
+     * @return {boolean}
+     */
+    isMatch(s, p) {
+        return this.matchHelper(s, p);
+    }
+
+    matchHelper(s, p, currS = 0, currP = 0, visited = {}){
+        if(visited[currS + '#' + currP] != undefined) return visited[currS + '#' + currP];
+        if(currS >= s.length && currP >= p.length) return true;
+        else if(currP >= p.length) return false;
+
+        let match = currS < s.length && (s[currS] === p[currP] || p[currP] === '.');
+
+        if(currP < p.length-1 && p[currP+1] === '*'){
+            if(match){
+                visited[currS + '#' + currP] = true && this.matchHelper(s, p, currS+1, currP, visited) || this.matchHelper(s,p, currS, currP+2, visited);
+            } else{
+                visited[currS + '#' + currP] = true && this.matchHelper(s, p, currS, currP+2, visited);
+            }
+        } else if(match){
+            visited[currS + '#' + currP] = true && this.matchHelper(s, p, currS+1, currP+1, visited);
+        } else{
+            visited[currS + '#' + currP] = false;
+        }
+
+        return visited[currS + '#' + currP];
+    }
+}
